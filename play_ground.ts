bf21b126e2c8ede382ee2eada9d44b2544ecc661
Mauricio Obando Gómez <mauricioobgo@gmail.com>
2020-06-24 09:33:43 -0500

    Interface in Typescritp

---
+++ play_ground.ts
@@ -0,0 +1,21 @@
+
+interface IPrueba{
+
+    texto:string
+    numero:number
+    boolenao:boolean
+    obj:{
+        texto:string
+        numero:number
+        booleano:boolean
+    }
+    arr:string[]
+    arrN:number[]
+    arrB: boolean[]
+
+    
+}
+
+const fn=(x:IPrueba)=>{
+    
+}
