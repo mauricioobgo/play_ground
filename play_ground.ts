acaa6e166f40c79c0aa550a7ae939271e9a6e68b
Mauricio Obando Gómez <mauricioobgo@gmail.com>
2020-06-24 09:43:34 -0500

    interface TypeScript2

--- play_ground.ts
+++ play_ground.ts
@@ -1,21 +1,42 @@
 
+
+// el signo ? sirve para definir que el argumento de la interface es opcional
 interface IPrueba{
 
     texto:string
-    numero:number
-    boolenao:boolean
-    obj:{
+    numero?:number
+    boolenao?:boolean
+    obj?:{
         texto:string
         numero:number
         booleano:boolean
     }
-    arr:string[]
-    arrN:number[]
-    arrB: boolean[]
-
-    
+    arr?:string[]
+    arrN?:number[]
+    arrB?: boolean[]
 }
 
-const fn=(x:IPrueba)=>{
-    
+interface IPrueba2{
+    met(x:string):number
+}
+
+interface IUser{
+    name:string
+}
+// con la finalidad de prevenir errores de tipo
+const fn=(x:IPrueba):IUser[]=>{
+    return [{name:""}]
+}
+
+fn({texto:''})
+
+class ClasePrueba implements IPrueba{
+    texto=""
+}
+
+class ClasePrueba2 implements IPrueba2{
+    public texto="texto"
+    public met(x){
+        return 1
+    }
 }
